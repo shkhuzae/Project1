cc7ca8848c6b155d819c89321ae2b4fc3120b088
shkhuzae <shouqalkhuzaei@gmail.com>
2019-02-14 14:51:32 +0000

    Added comments to the database.rb file

--- database.rb
+++ database.rb
@@ -1,20 +1,33 @@
+#requiring external sqlite3 module that is installed 
 require 'sqlite3'
 
+#defining class DBHandler
 class DBHandler
+  
+  #constructor 
    def initialize
+     #error catching
       begin
+        #opening student.db database
         db = SQLite3::Database.open "student.db"
+        
+        #creating Students table with required parameters
         dbstatement = "CREATE TABLE IF NOT EXISTS Students(ID INTEGER PRIMARY KEY, 
         firstname TEXT, lastname TEXT, major TEXT, email TEXT, status TEXT);"
+        
+        #executing statement
         db.execute dbstatement
+        
       rescue SQLite3::Exception => e
         puts "Exception Occurred"
         puts e
       ensure
         db.close if db
       end
+      #end of error catching
    end
    
+   #inserting values into Students table
    def create(firstname, lastname, major, email, status)
       begin
         db = SQLite3::Database.open "student.db"
@@ -28,6 +41,7 @@ class DBHandler
       end
    end   
    
+   #selecting all students
    def all
       begin
         db = SQLite3::Database.open "student.db"
@@ -41,6 +55,7 @@ class DBHandler
       end
    end   
    
+   #selecting enrolled or graduated students depending on the parameter
    def enrolled(status)
       begin
         db = SQLite3::Database.open "student.db"
@@ -54,7 +69,7 @@ class DBHandler
       end
    end   
    
-   
+   #selecting a student's record based on their ID
    def get(id)
       begin
         db = SQLite3::Database.open "student.db"
@@ -68,6 +83,7 @@ class DBHandler
       end
    end    
   
+   #updating a student's information based on given parameters
    def update(id, firstname, lastname, major, email, status)
       begin
         db = SQLite3::Database.open "student.db"
@@ -81,6 +97,7 @@ class DBHandler
       end
    end   
    
+   #deleting a student's record from the table Students
    def destroy(id)
       begin
         db = SQLite3::Database.open "student.db"
